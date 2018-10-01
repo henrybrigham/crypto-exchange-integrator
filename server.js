d7ce8058de146737f9a8b4a10955026a91514729
Henry Brigham <hhbrigham5@gmail.com>
2018-10-01 01:26:11 -0600

    going to sleep mode

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/'));
-app.listen(process.env.PORT);
-console.log('listening', process.env.PORT);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+console.log('listening', PORT);
